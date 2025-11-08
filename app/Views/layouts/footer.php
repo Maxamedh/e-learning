33d704814a7a8c3ba6d6c079fa456ef52cc37a1c
maxamedh <maxamedxsn24@gmail.com>
2025-11-08 07:50:36 +0300

    2025-011-08

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,13 @@
+ <!-- Footer -->
+             <div class="footer text-center bg-white shadow-sm py-3 mt-5">
+                <p class="m-0">Copyright © 2024. All Rights Reserved. <a href="https://www.templaterise.com/" class="text-primary" target="_blank" >Themes By TemplateRise</a></p>
+            </div>
+    </div>
+     <!-- Scripts -->
+    <script  src="<?= base_url('assets/js/jquery-3.6.0.min.js')?>"></script>
+    <script  src="<?= base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+    <script  src="<?= base_url('assets/plugin/chart/chart.js')?>"></script>
+    <script  src="<?= base_url('assets/js/chart.js')?>"></script>
+    <script  src="<?= base_url('assets/js/main.js')?>"></script>
+</body>
+</html>
